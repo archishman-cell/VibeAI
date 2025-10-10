894cb7507d26c7fb5493191271ce27ca538162a3
archishman-cell <archishmankar8@gmail.com>
2025-10-10 21:11:50 +0530

    .

--- src/components/AboutModal.jsx
+++ src/components/AboutModal.jsx
@@ -101,8 +101,8 @@ const AboutModal = ({ isOpen, onClose }) => {
                 <span>Developer Profile</span>
               </a>
               <a 
-                href="" 
-                target="https://www.linkedin.com/in/archishman-kar-15131336b" 
+                href="https://www.linkedin.com/in/archishman-kar-15131336b" 
+                target="_blank" 
                 rel="noopener noreferrer"
                 className="flex items-center space-x-2 text-sm text-gray-600 hover:text-gray-900 hover:underline transition-colors"
                 aria-label="Developer's LinkedIn Profile"
